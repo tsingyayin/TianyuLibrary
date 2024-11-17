3e11ade2f64dd88f74f5e90b67711d828941b436
Yayin Tsing <tsingyayin@163.com>
2024-11-17 22:16:07 +0800

    init

--- src/main/java/cn/yxgeneral/weavestudio/tianyulibrary/TianyuLibrary.java
+++ src/main/java/cn/yxgeneral/weavestudio/tianyulibrary/TianyuLibrary.java
@@ -3,15 +3,32 @@ package cn.yxgeneral.weavestudio.tianyulibrary;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TianyuLibrary extends JavaPlugin {
-
+    static TianyuLibrary Instance;
+    static boolean PAPI = false;
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Tianyu Plugin Library is enabled!");
+        if (getServer().getPluginManager().getPlugin("PlaceHolderAPI")!=null){
+            info("PlaceHolderAPI found, enabling PlaceHolderAPI support...");
+            PAPI = true;
+        }else{
+            warning("PlaceHolderAPI not found, disabling PlaceHolderAPI support...");
+            PAPI = false;
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    public static TianyuLibrary getInstance() {
+        return Instance;
+    }
+    public static void info(String info){
+        Instance.getLogger().info(info);
+    }
+    public static void warning(String warning){
+        Instance.getLogger().warning(warning);
     }
 }
